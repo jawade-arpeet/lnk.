5c2373e91bfe79d50941b49605d7fb33ee7719f7
jawade.arpeet <jawade.arpeet@gmail.com>
2024-11-26 16:39:38 +0530

    add css variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
